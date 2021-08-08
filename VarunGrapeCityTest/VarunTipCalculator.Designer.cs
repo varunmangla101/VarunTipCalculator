1cf009a0c71f388ea0434cec1a810d77e3500b98
varunmangla101 <varunmangla101@gmail.com>
2021-08-08 15:14:19 +0530

    Tip Calculator completed

---
+++ VarunGrapeCityTest/VarunTipCalculator.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace VarunGrapeCityTest
+{
+    partial class VarunTipCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BillLabel = new System.Windows.Forms.Label();
+            this.TipLabel = new System.Windows.Forms.Label();
+            this.PeopleLabel = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.TipPerPersonAmountLabel = new System.Windows.Forms.Label();
+            this.TotalPerPersonAmountLabel = new System.Windows.Forms.Label();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.LineLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BillLabel
+            // 
+            this.BillLabel.AutoSize = true;
+            this.BillLabel.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BillLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BillLabel.Location = new System.Drawing.Point(21, 32);
+            this.BillLabel.Name = "BillLabel";
+            this.BillLabel.Size = new System.Drawing.Size(41, 30);
+            this.BillLabel.TabIndex = 0;
+            this.BillLabel.Text = "Bill";
+            // 
+            // TipLabel
+            // 
+            this.TipLabel.AutoSize = true;
+            this.TipLabel.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TipLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.TipLabel.Location = new System.Drawing.Point(21, 149);
+            this.TipLabel.Name = "TipLabel";
+            this.TipLabel.Size = new System.Drawing.Size(67, 30);
+            this.TipLabel.TabIndex = 2;
+            this.TipLabel.Text = "Tip %";
+            // 
+            // PeopleLabel
+            // 
+            this.PeopleLabel.AutoSize = true;
+            this.PeopleLabel.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PeopleLabel.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.PeopleLabel.Location = new System.Drawing.Point(21, 278);
+            this.PeopleLabel.Name = "PeopleLabel";
+            this.PeopleLabel.Size = new System.Drawing.Size(193, 30);
+            this.PeopleLabel.TabIndex = 3;
+            this.PeopleLabel.Text = "Number of people";
+            // 
+            // Label1
+            // 
+            this.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label1.Location = new System.Drawing.Point(76, 196);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(193, 50);
+            this.Label1.TabIndex = 4;
+            this.Label1.Text = "0%";
+            this.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label3.Location = new System.Drawing.Point(441, 112);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(66, 46);
+            this.Label3.TabIndex = 6;
+            this.Label3.Text = "Tip";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Label4.Location = new System.Drawing.Point(438, 154);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(136, 35);
+            this.Label4.TabIndex = 7;
+            this.Label4.Text = "per person";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Label6.Location = new System.Drawing.Point(438, 280);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(136, 35);
+            this.Label6.TabIndex = 9;
+            this.Label6.Text = "per person";
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label5.Location = new System.Drawing.Point(442, 241);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(92, 46);
+            this.Label5.TabIndex = 8;
+            this.Label5.Text = "Total";
+            // 
+            // TipPerPersonAmountLabel
+            // 
+            this.TipPerPersonAmountLabel.AutoSize = true;
+            this.TipPerPersonAmountLabel.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TipPerPersonAmountLabel.Location = new System.Drawing.Point(687, 113);
+            this.TipPerPersonAmountLabel.Name = "TipPerPersonAmountLabel";
+            this.TipPerPersonAmountLabel.Size = new System.Drawing.Size(142, 78);
+            this.TipPerPersonAmountLabel.TabIndex = 10;
+            this.TipPerPersonAmountLabel.Text = "$0.0";
+            // 
+            // TotalPerPersonAmountLabel
+            // 
+            this.TotalPerPersonAmountLabel.AutoSize = true;
+            this.TotalPerPersonAmountLabel.Font = new System.Drawing.Font("Segoe UI", 47F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TotalPerPersonAmountLabel.Location = new System.Drawing.Point(687, 219);
+            this.TotalPerPersonAmountLabel.Name = "TotalPerPersonAmountLabel";
+            this.TotalPerPersonAmountLabel.Size = new System.Drawing.Size(201, 105);
+            this.TotalPerPersonAmountLabel.TabIndex = 11;
+            this.TotalPerPersonAmountLabel.Text = "$0.0";
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TextBox1.Location = new System.Drawing.Point(26, 72);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(290, 47);
+            this.TextBox1.TabIndex = 12;
+            this.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Button2
+            // 
+            this.Button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Button2.Location = new System.Drawing.Point(26, 196);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(52, 50);
+            this.Button2.TabIndex = 13;
+            this.Button2.Text = "-";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.decrementTipButtonClicked);
+            // 
+            // Button1
+            // 
+            this.Button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Button1.Location = new System.Drawing.Point(266, 196);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(50, 50);
+            this.Button1.TabIndex = 14;
+            this.Button1.Text = "+";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.incrementTipButtonClicked);
+            // 
+            // Button3
+            // 
+            this.Button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Button3.Location = new System.Drawing.Point(264, 316);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(52, 52);
+            this.Button3.TabIndex = 17;
+            this.Button3.Text = "+";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.incrementPeopleButtonClicked);
+            // 
+            // Button4
+            // 
+            this.Button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Button4.Location = new System.Drawing.Point(26, 316);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(52, 52);
+            this.Button4.TabIndex = 16;
+            this.Button4.Text = "-";
+            this.Button4.UseVisualStyleBackColor = true;
+            this.Button4.Click += new System.EventHandler(this.decrementPeopleButtonClicked);
+            // 
+            // Label2
+            // 
+            this.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Label2.Location = new System.Drawing.Point(76, 318);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(193, 50);
+            this.Label2.TabIndex = 15;
+            this.Label2.Text = "1";
+            this.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LineLabel
+            // 
+            this.LineLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.LineLabel.Location = new System.Drawing.Point(372, -1);
+            this.LineLabel.Name = "LineLabel";
+            this.LineLabel.Size = new System.Drawing.Size(10, 456);
+            this.LineLabel.TabIndex = 18;
+            // 
+            // VarunTipCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1149, 452);
+            this.Controls.Add(this.LineLabel);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.TotalPerPersonAmountLabel);
+            this.Controls.Add(this.TipPerPersonAmountLabel);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.PeopleLabel);
+            this.Controls.Add(this.TipLabel);
+            this.Controls.Add(this.BillLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "VarunTipCalculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BillLabel;
+        private System.Windows.Forms.Label TipLabel;
+        private System.Windows.Forms.Label PeopleLabel;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.Label Label6;
+        private System.Windows.Forms.Label Label5;
+        private System.Windows.Forms.Label TipPerPersonAmountLabel;
+        private System.Windows.Forms.Label TotalPerPersonAmountLabel;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label LineLabel;
+    }
+}
+
---
+++ VarunGrapeCityTest/VarunTipCalculator.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VarunGrapeCityTest
+{
+    public partial class VarunTipCalculator : Form
+    {
+        public VarunTipCalculator()
+        {
+            InitializeComponent();
+        }
+
+        // Method to Reset all the fields and set them to their initial values when an invalid move occurs.
+        private void ResetFields()
+        {
+            TextBox1.Text = "";
+            Label1.Text = "0%";
+            Label2.Text = "1";
+            double num = 0.0;
+            TipPerPersonAmountLabel.Text = num.ToString("C");
+            TotalPerPersonAmountLabel.Text = num.ToString("C");
+        }
+
+        // Method to check whether the user has entered any value in Bill box or not.
+        // If box is empty, user is given a message that Bill amount cannot be left blank.
+        private bool isBillBlank()
+        {
+            if (TextBox1.Text == "")
+            {
+                ResetFields();
+                MessageBox.Show("Bill amount cannot be blank");
+                return true;
+            }
+            return false;
+        }
+
+
+        // Method to show error message when bill amount entered is less than or equal to 0.
+        private void printBillRangeMsg()
+        {
+            ResetFields();
+            MessageBox.Show("Bill cannot be less than or equal to 0");
+        }
+
+        // Method to show error message when bill amount entered is not in proper for-mat.
+        // For example, any alphabet or special character entered instead of numbers.
+        private void printBillInvalidMsg()
+        {
+            ResetFields();
+            MessageBox.Show("Bill amount invalid");
+        }
+
+        // Method to calculate the Tip per person and display it.
+        private void tipPerPerson(double billAmount)
+        {
+            double tipPerPerson, intermediateResult;
+            int noOfPeople = getNoOfPeople();
+            int tipPercent = getTipPercent();
+
+            intermediateResult = (billAmount * tipPercent) / 100;
+            tipPerPerson = intermediateResult / noOfPeople;
+            TipPerPersonAmountLabel.Text = tipPerPerson.ToString("C");
+        }
+
+        // Method to calculate the Total per person and display it.
+        private void totalPerPerson(double billAmount)
+        {
+            double totalPerPerson, intermediateResult;
+            int noOfPeople = getNoOfPeople();
+            int tipPercent = getTipPercent();
+
+            intermediateResult = (billAmount * tipPercent) / 100;
+            totalPerPerson = (billAmount + intermediateResult) / noOfPeople;
+            TotalPerPersonAmountLabel.Text = totalPerPerson.ToString("C");
+        }
+
+        // Method to retrieve the Tip Percentage entered from its respective box and returns
+        // it to the calling method.
+        // This method converts string input of Tip percentage into its equivalent nu-meric value
+        // and returns that value.
+        private int getTipPercent()
+        {
+            string textOfTipBox = Label1.Text;
+            string numInTipBox = "";
+            int val = 0;
+
+            for (int i = 0; i < textOfTipBox.Length; i++)
+            {
+                if (Char.IsDigit(textOfTipBox[i]))
+                {
+                    numInTipBox += textOfTipBox[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            val = int.Parse(numInTipBox);
+            return val;
+        }
+
+        // Method to retrieve the Number of people entered from its respective box and returns
+        // it to the calling method.
+        // This method converts string input of Number of people into its equivalent numeric value
+        // and returns that value.
+        private int getNoOfPeople()
+        {
+            int val = Convert.ToInt32(Label2.Text);
+            return val;
+        }
+
+
+        // This method gets invoked when user presses the + button for increasing the Tip
+        // percentage by 1% (each time).
+        // It first checks the bill amount is not empty by calling the method "is-BillBlank()".
+        // Then it converts and simultaneously checks for any invalid amount entered.
+        private void incrementTipButtonClicked(object sender, EventArgs e)
+        {
+            if (!isBillBlank())
+            {
+                try
+                {
+                    double billAmount = Convert.ToDouble(TextBox1.Text);
+
+                    // Checking if Bill is less than or equal to 0, then print that respective error message.
+                    if (billAmount <= 0)
+                    {
+                        printBillRangeMsg();
+                    }
+                    else
+                    {
+                        int val = getTipPercent();
+                        ++val;
+                        Label1.Text = val.ToString() + "%";
+
+                        // Tip increased by 1% and calculating new Tip per person and Total per person with new value
+                        tipPerPerson(billAmount);
+                        totalPerPerson(billAmount);
+                    }
+                }
+                catch (FormatException exc)
+                {
+                    // Handling exception in case any invalid amount is entered in Bill box
+                    printBillInvalidMsg();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Some error occurred. Try again!");
+                    ResetFields();
+                }
+            }
+        }
+
+        // This method gets invoked when user presses the - button for decreasing the Tip
+        // percentage by 1% (each time).
+        // It first checks the bill amount is not empty by calling the method "is-BillBlank()".
+        // Then it converts and simultaneously checks for any invalid amount entered.
+        protected void decrementTipButtonClicked(object sender, EventArgs e)
+        {
+            if (!isBillBlank())
+            {
+                try
+                {
+                    double billAmount = Convert.ToDouble(TextBox1.Text);
+
+                    // Checking if Bill is less than or equal to 0, then print that respective error message.
+                    if (billAmount <= 0)
+                    {
+                        printBillRangeMsg();
+                    }
+                    else
+                    {
+                        int val = getTipPercent();
+
+                        // Retrieving current value of Tip % before decrementing and if it is 0, then decrementing further
+                        // is not possible and showing respective error message.
+                        if (val == 0)
+                        {
+                            MessageBox.Show("Tip cannot be less than 0% !");
+                            ResetFields();
+                        }
+                        else
+                        {
+                            --val;
+                            Label1.Text = val.ToString() + "%";
+
+                            // Tip decreased by 1% and calculating new Tip per person and Total per person with new value
+                            tipPerPerson(billAmount);
+                            totalPerPerson(billAmount);
+                        }
+                    }
+                }
+                catch (FormatException exc)
+                {
+                    // Handling exception in case any invalid amount is entered in Bill box
+                    printBillInvalidMsg();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Some error occurred. Try again!");
+                    ResetFields();
+                }
+            }
+        }
+
+
+        // This method gets invoked when user presses the + button for increasing the Number
+        // of people by 1 (each time).
+        // It first checks the bill amount is not empty by calling the method "is-BillBlank()".
+        // Then it converts and simultaneously checks for any invalid amount entered.
+        protected void incrementPeopleButtonClicked(object sender, EventArgs e)
+        {
+            if (!isBillBlank())
+            {
+                try
+                {
+                    double billAmount = Convert.ToDouble(TextBox1.Text);
+
+                    // Checking if Bill is less than or equal to 0, then print that respective error message.
+                    if (billAmount <= 0)
+                    {
+                        printBillRangeMsg();
+                    }
+                    else
+                    {
+                        int val = getNoOfPeople();
+                        ++val;
+                        Label2.Text = val.ToString();
+
+                        // Number of people increased by 1 and calculating new Tip per person and Total per person with new value
+                        tipPerPerson(billAmount);
+                        totalPerPerson(billAmount);
+                    }
+                }
+                catch (FormatException exc)
+                {
+                    // Handling exception in case any invalid amount is entered in Bill box
+                    printBillInvalidMsg();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Some error occurred. Try again!");
+                    ResetFields();
+                }
+            }
+        }
+
+        // This method gets invoked when user presses the - button for decreasing the Number
+        // of people by 1 (each time).
+        // It first checks the bill amount is not empty by calling the method "is-BillBlank()".
+        // Then it converts and simultaneously checks for any invalid amount entered.
+        protected void decrementPeopleButtonClicked(object sender, EventArgs e)
+        {
+            if (!isBillBlank())
+            {
+                try
+                {
+                    double billAmount = Convert.ToDouble(TextBox1.Text);
+
+                    // Checking if Bill is less than or equal to 0, then print that respective error message.
+                    if (billAmount <= 0)
+                    {
+                        printBillRangeMsg();
+                    }
+                    else
+                    {
+                        int val = getNoOfPeople();
+
+                        // Retrieving current value of Number of people before decre-menting and if it is 1, then decrementing further
+                        // is not possible as minimum no. of people should be at least 1 to give tip to them, and showing
+                        // respective error message.
+                        if (val == 1)
+                        {
+                            MessageBox.Show("No. of people cannot be less than 1!");
+                            ResetFields();
+                        }
+                        else
+                        {
+                            --val;
+                            Label2.Text = val.ToString();
+
+                            // Number of people decreased by 1 and calculating new Tip per person and Total per person with new value
+                            tipPerPerson(billAmount);
+                            totalPerPerson(billAmount);
+                        }
+                    }
+                }
+                catch (FormatException exc)
+                {
+                    // Handling exception in case any invalid amount is entered in Bill box
+                    printBillInvalidMsg();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Some error occurred. Try again!");
+                    ResetFields();
+                }
+            }
+        }
+    }
+}
